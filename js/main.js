1b3f81a19bee32a89bd16484843d13df2721b203
Leroy Ayettey <119285013+leroyayettey@users.noreply.github.com>
2026-04-03 11:24:31 -0400

    Updated main.js album info function

--- js/main.js
+++ js/main.js
@@ -1,33 +1,13 @@
-// -----------------------------------------------------------------------------
-// This file includes deliberate formatting errors in order for you to verify
-// that ESLint and EditorConfig are working properly. If both tools are, indeed,
-// working correctly, then you’d see errors in your editor about indentation and
-// improper use of footmarks instead of back ticks. When you save this file,
-// your editor should strip all excess newlines and whitespace characters from
-// the file. If both of these events occur, then ESLint and EditorConfig are
-// working correctly.
-//
-// DON’T PROCEED UNTIL YOU’RE SURE ESLINT AND EDITORCONFIG ARE WORKING CORRECTLY
-// -----------------------------------------------------------------------------
-window.onload = () => {
-       alert('Test');                    
+body = document.querySelector('body');
+
+
+
+function albumInfo(data){
+    artistName = data.artist;
+    albumName = data.album;
+    year = data.year;
+    cover = data.cover;
+    description = data.description;
+    rating = data.rating;
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
