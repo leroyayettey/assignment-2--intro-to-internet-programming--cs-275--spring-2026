e66078b6feb22ab197f8b32ee584a07a560f1bb5
leroyayettey <119285013+leroyayettey@users.noreply.github.com>
2026-04-03 16:26:22 -0400

    Updated main.js with carousel element reiteration

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
-body = document.querySelector('body');
+let body = document.querySelector('body');
 
 
+let cyclingAlbums = (data) => {
+    let carousel = document.querySelector('.carousel-slides');
 
-function albumInfo(data){
-    artistName = data.artist;
-    albumName = data.album;
-    year = data.year;
-    cover = data.cover;
-    description = data.description;
-    rating = data.rating;
+    for (let i = 0; i < data.length; i++)
+    {
+        let artistName = data[i].artist;
+        let artistURL = data[i].url;
+        let albumName = data[i].album;
+        let albumCover = data[i].cover_image;
+        let albumReview = data[i].review.content;
+
+        let slide = document.createElement('div');
+        slide.classList.add('slide');
+
+        let albumTitle = document.createElement('h2');
+        albumTitle.textContent = `${albumName}`;
+        slide.appendChild(albumTitle)
+
+        let albumArtist = document.createElement('a')
+        albumArtist.href = artistURL;
+        albumArtist.target = '_blank';
+        albumArtist.textContent = artistName;
+
+        let albumImg = document.createElement('img');
+        albumImg.src = albumCover.path;
+        albumImg.alt = albumCover.alt_content;
+        slide.appendChild(albumImg);
+
+    }
 
 }
+
+const script = document.createElement(`script`);
+script.setAttribute(`src`, `json/data.json`);
+carousel-slides.appendChild(script);
